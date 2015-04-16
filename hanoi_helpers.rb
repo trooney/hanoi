73c88c2700f73fe84b3fa3606330e57db70d27ad
Tyler Rooney <tyler@tylerrooney.ca>
2015-04-16 14:26:02 -0300

    Adding tip

--- hanoi_helpers.rb
+++ hanoi_helpers.rb
@@ -1,10 +1,19 @@
-def render(game)
+def render(game, showTip = false)
 	print "\e[H\e[2J"
 	puts "Tower of Hanoi"
 	puts
 	print View.render_board(game.board)
-	puts
+
+  if showTip
+  	puts
+    print "\nTip: To move ring from first to second tower"
+    print "\n     Type '1 2' and pressing enter."
+    print "\n     (Invalid moves are ignored)"
+  end
+
+  puts
 	print "\n# #{game.move_counter} (t1, t2) > "
+
 end
 
 def parse_move(input)
